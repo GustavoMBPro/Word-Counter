a343267548724d6d0bfdb6008e4c56cb6c658a08
GustavoMBPro <105040306+GustavoMBPro@users.noreply.github.com>
2022-05-06 18:51:41 -0500

    Add files via upload

---
+++ Activity8WordCounter/Activity8WordCounter/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace Activity8WordCounter
+{
+    partial class WordCounter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WordCounter));
+            this.richTextBoxSentence = new System.Windows.Forms.RichTextBox();
+            this.labelWordCounter = new System.Windows.Forms.Label();
+            this.labelNumberOfWords = new System.Windows.Forms.Label();
+            this.labelAvgNumber = new System.Windows.Forms.Label();
+            this.labelSetenceCapitalizer = new System.Windows.Forms.Label();
+            this.richTextBoxCapitalizeSentence = new System.Windows.Forms.RichTextBox();
+            this.labelDisplayNumberOfWords = new System.Windows.Forms.Label();
+            this.labelDisplayAvhNumberOfWords = new System.Windows.Forms.Label();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.checkBoxNumberExclusion = new System.Windows.Forms.CheckBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBoxSentence
+            // 
+            this.richTextBoxSentence.Location = new System.Drawing.Point(143, 76);
+            this.richTextBoxSentence.Name = "richTextBoxSentence";
+            this.richTextBoxSentence.Size = new System.Drawing.Size(407, 124);
+            this.richTextBoxSentence.TabIndex = 1;
+            this.richTextBoxSentence.Text = "";
+            // 
+            // labelWordCounter
+            // 
+            this.labelWordCounter.AutoSize = true;
+            this.labelWordCounter.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.labelWordCounter.Font = new System.Drawing.Font("Bell MT", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWordCounter.Location = new System.Drawing.Point(165, 9);
+            this.labelWordCounter.Name = "labelWordCounter";
+            this.labelWordCounter.Size = new System.Drawing.Size(360, 54);
+            this.labelWordCounter.TabIndex = 2;
+            this.labelWordCounter.Text = "Enter a Sentence";
+            // 
+            // labelNumberOfWords
+            // 
+            this.labelNumberOfWords.AutoSize = true;
+            this.labelNumberOfWords.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.labelNumberOfWords.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNumberOfWords.Location = new System.Drawing.Point(139, 215);
+            this.labelNumberOfWords.Name = "labelNumberOfWords";
+            this.labelNumberOfWords.Size = new System.Drawing.Size(133, 20);
+            this.labelNumberOfWords.TabIndex = 3;
+            this.labelNumberOfWords.Text = "Number of words:";
+            // 
+            // labelAvgNumber
+            // 
+            this.labelAvgNumber.AutoSize = true;
+            this.labelAvgNumber.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.labelAvgNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAvgNumber.Location = new System.Drawing.Point(139, 249);
+            this.labelAvgNumber.Name = "labelAvgNumber";
+            this.labelAvgNumber.Size = new System.Drawing.Size(202, 20);
+            this.labelAvgNumber.TabIndex = 4;
+            this.labelAvgNumber.Text = "Average number of Letters:";
+            // 
+            // labelSetenceCapitalizer
+            // 
+            this.labelSetenceCapitalizer.AutoSize = true;
+            this.labelSetenceCapitalizer.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.labelSetenceCapitalizer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSetenceCapitalizer.Location = new System.Drawing.Point(139, 281);
+            this.labelSetenceCapitalizer.Name = "labelSetenceCapitalizer";
+            this.labelSetenceCapitalizer.Size = new System.Drawing.Size(161, 20);
+            this.labelSetenceCapitalizer.TabIndex = 5;
+            this.labelSetenceCapitalizer.Text = "Capitalized sentence:";
+            // 
+            // richTextBoxCapitalizeSentence
+            // 
+            this.richTextBoxCapitalizeSentence.Location = new System.Drawing.Point(143, 308);
+            this.richTextBoxCapitalizeSentence.Name = "richTextBoxCapitalizeSentence";
+            this.richTextBoxCapitalizeSentence.ReadOnly = true;
+            this.richTextBoxCapitalizeSentence.Size = new System.Drawing.Size(407, 124);
+            this.richTextBoxCapitalizeSentence.TabIndex = 6;
+            this.richTextBoxCapitalizeSentence.Text = "";
+            // 
+            // labelDisplayNumberOfWords
+            // 
+            this.labelDisplayNumberOfWords.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelDisplayNumberOfWords.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelDisplayNumberOfWords.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDisplayNumberOfWords.Location = new System.Drawing.Point(289, 214);
+            this.labelDisplayNumberOfWords.Name = "labelDisplayNumberOfWords";
+            this.labelDisplayNumberOfWords.Size = new System.Drawing.Size(133, 20);
+            this.labelDisplayNumberOfWords.TabIndex = 7;
+            // 
+            // labelDisplayAvhNumberOfWords
+            // 
+            this.labelDisplayAvhNumberOfWords.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelDisplayAvhNumberOfWords.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelDisplayAvhNumberOfWords.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDisplayAvhNumberOfWords.Location = new System.Drawing.Point(358, 249);
+            this.labelDisplayAvhNumberOfWords.Name = "labelDisplayAvhNumberOfWords";
+            this.labelDisplayAvhNumberOfWords.Size = new System.Drawing.Size(133, 20);
+            this.labelDisplayAvhNumberOfWords.TabIndex = 8;
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEnter.Location = new System.Drawing.Point(27, 393);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(92, 33);
+            this.buttonEnter.TabIndex = 9;
+            this.buttonEnter.Text = "Enter";
+            this.buttonEnter.UseVisualStyleBackColor = false;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.Location = new System.Drawing.Point(578, 393);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(92, 33);
+            this.buttonExit.TabIndex = 10;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // checkBoxNumberExclusion
+            // 
+            this.checkBoxNumberExclusion.AutoSize = true;
+            this.checkBoxNumberExclusion.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.checkBoxNumberExclusion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxNumberExclusion.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.checkBoxNumberExclusion.Location = new System.Drawing.Point(439, 214);
+            this.checkBoxNumberExclusion.Name = "checkBoxNumberExclusion";
+            this.checkBoxNumberExclusion.Size = new System.Drawing.Size(190, 24);
+            this.checkBoxNumberExclusion.TabIndex = 11;
+            this.checkBoxNumberExclusion.Text = "Do not count numbers.";
+            this.checkBoxNumberExclusion.UseVisualStyleBackColor = false;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClear.Location = new System.Drawing.Point(27, 354);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(92, 33);
+            this.buttonClear.TabIndex = 12;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = false;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // WordCounter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(728, 444);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.checkBoxNumberExclusion);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.labelDisplayAvhNumberOfWords);
+            this.Controls.Add(this.labelDisplayNumberOfWords);
+            this.Controls.Add(this.richTextBoxCapitalizeSentence);
+            this.Controls.Add(this.labelSetenceCapitalizer);
+            this.Controls.Add(this.labelAvgNumber);
+            this.Controls.Add(this.labelNumberOfWords);
+            this.Controls.Add(this.labelWordCounter);
+            this.Controls.Add(this.richTextBoxSentence);
+            this.Name = "WordCounter";
+            this.Text = "Word Counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxSentence;
+        private System.Windows.Forms.Label labelWordCounter;
+        private System.Windows.Forms.Label labelNumberOfWords;
+        private System.Windows.Forms.Label labelAvgNumber;
+        private System.Windows.Forms.Label labelSetenceCapitalizer;
+        private System.Windows.Forms.RichTextBox richTextBoxCapitalizeSentence;
+        private System.Windows.Forms.Label labelDisplayNumberOfWords;
+        private System.Windows.Forms.Label labelDisplayAvhNumberOfWords;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.CheckBox checkBoxNumberExclusion;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ Activity8WordCounter/Activity8WordCounter/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity8WordCounter
+{
+    public partial class WordCounter : Form
+    {
+
+        /*Author: Gustavo Marin Borges
+         Title: WordCount*/
+
+        public WordCounter()
+        {
+            InitializeComponent();
+        }
+
+        //Enter button.
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+            
+            int NoNumbers = 0;
+
+            //CheckBox value.
+            if (checkBoxNumberExclusion.Checked)
+            {
+                NoNumbers = DoNotCountNumbers();
+            }
+
+            //Tries for extra spaces or the absence of data.
+            try
+            {
+                string xUpper = UpperCase();
+                richTextBoxCapitalizeSentence.Text = xUpper;
+
+                labelDisplayNumberOfWords.Text = (WordCounterMethod() + NoNumbers).ToString();
+
+                //Verifies that the average is not negative.
+                if ((AverageNumberOfLettersMethod() + NoNumbers) > 0)
+                {
+                    labelDisplayAvhNumberOfWords.Text = (AverageNumberOfLettersMethod() + NoNumbers).ToString();
+                }
+                else
+                {
+                    labelDisplayNumberOfWords.Text = "0";
+                    labelDisplayAvhNumberOfWords.Text = "0";
+                }
+                
+            }
+            catch
+            {
+                //Shows error message.
+                MessageBox.Show("There is an extra space in your sentence or there is nothing in it.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+         
+            
+            
+        }
+
+        //Method for counting words.
+        private int WordCounterMethod()
+        {
+            string strCount = richTextBoxSentence.Text;
+            int count = 0;
+            int index = 1;
+            char[] delim = null;
+            string[] tokens = strCount.Split(delim);
+            count = tokens.Length;
+
+           
+
+            return count;
+        }
+
+        //Method for not counting numbers.
+        private int DoNotCountNumbers()
+        {
+            string strCount = richTextBoxSentence.Text;
+            int count = 0;
+            int index = 0;
+            int i = 1;
+            char[] delim = null;
+            string[] tokens = strCount.Split(delim);
+
+            while (index <= (tokens.Length - 1))
+            {
+                if (tokens[index].StartsWith("0") || tokens[index].EndsWith("0"))
+                {
+                    count--;
+                }
+                else if (tokens[index].StartsWith("1") || tokens[index].EndsWith("1"))
+                {
+                    count--;
+                }
+                else if (tokens[index].StartsWith("2") || tokens[index].EndsWith("2"))
+                {
+                    count--;
+                }
+                else if (tokens[index].StartsWith("3") || tokens[index].EndsWith("3"))
+                {
+                    count--;
+                }
+                else if (tokens[index].StartsWith("4") || tokens[index].EndsWith("4"))
+                {
+                    count--;
+                }
+                else if (tokens[index].StartsWith("5") || tokens[index].EndsWith("5"))
+                {
+                    count--;
+                }
+                else if (tokens[index].StartsWith("6") || tokens[index].EndsWith("6"))
+                {
+                    count--;
+                }
+                else if (tokens[index].StartsWith("7") || tokens[index].EndsWith("7"))
+                {
+                    count--;
+                }
+                else if (tokens[index].StartsWith("8") || tokens[index].EndsWith("8"))
+                {
+                    count--;
+                }
+                else if (tokens[index].StartsWith("9") || tokens[index].EndsWith("9"))
+                {
+                    count--;
+                }
+                index++;
+            }
+            return count;
+        }
+
+        //Method that calculates average number of words.
+        private double AverageNumberOfLettersMethod()
+        {
+            string strCount = richTextBoxSentence.Text;
+            double Avg = 0;
+            int Sum = 0;
+            int denominator = 1;
+
+            char[] delim = null;
+            string[] tokens = strCount.Split(delim);
+
+            for (int i = 0; i <= tokens.Length -1 ; i++)
+            {              
+               Sum = Sum + tokens[i].Length;
+               denominator++;
+            }
+
+            double Sum2 = Convert.ToDouble(Sum);
+            Avg = Sum2/ denominator;
+            return Avg;
+        }
+
+        //Method that turns sentence into first upper letter of each word.
+        private string UpperCase()
+        {
+            string Sum = "";
+            
+                string strCount = richTextBoxSentence.Text;                
+                char[] delim = null;
+                string[] tokens = strCount.Split(delim);
+
+                for (int i = 0; i <= tokens.Length - 1; i++)
+                {
+                    Sum = Sum + tokens[i].Substring(0, 1).ToUpper() + tokens[i].Substring(1) + " ";
+                }
+                                                      
+            return Sum;
+        }
+
+        //Exit button.
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //Clear button.
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            richTextBoxSentence.Text = "";
+            labelDisplayNumberOfWords.Text = "";
+            labelDisplayAvhNumberOfWords.Text = "";
+            richTextBoxCapitalizeSentence.Text = "";
+            checkBoxNumberExclusion.Checked = false;
+        }
+    }
+}
